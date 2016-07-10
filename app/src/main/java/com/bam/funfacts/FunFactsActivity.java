ea48b65c714477763f774127ef78554e394cc46f
PrintHelloWorld <blake.mccawe@gmail.com>
2016-07-10 22:55:10 +1200

    Set up onclicklistener for fact button to swap out a static fact

--- app/src/main/java/com/bam/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/bam/funfacts/FunFactsActivity.java
@@ -2,12 +2,29 @@ package com.bam.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class FunFactsActivity extends AppCompatActivity {
+public class FunFactsActivity extends AppCompatActivity implements OnCli{
+
+    private TextView mFactTextView;
+    private Button mNewFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mNewFactButton = (Button) findViewById(R.id.showFactButton);
+
+        mNewFactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Fact = "Ostriches can run faster than horses.";
+                mFactTextView.setText(Fact);
+            }
+        });
     }
 }
