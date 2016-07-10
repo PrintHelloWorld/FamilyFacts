9f877d5cc1f1a4b6525468a3ea7f7c4ceca87f3c
PrintHelloWorld <blake.mccawe@gmail.com>
2016-07-10 23:33:22 +1200

    Dynamically populate fact from array of facts

--- app/src/main/java/com/bam/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/bam/funfacts/FunFactsActivity.java
@@ -6,10 +6,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class FunFactsActivity extends AppCompatActivity implements OnCli{
+import java.util.Random;
+
+public class FunFactsActivity extends AppCompatActivity {
 
     private TextView mFactTextView;
     private Button mNewFactButton;
+
+    private final String[] facts = {
+            "Ants stretch when they wake up in the morning.",
+            "Ostriches can run faster than horses.",
+            "Olympic gold medals are actually made mostly of silver.",
+            "You are born with 300 bones; by the time you are an adult you will have 206.",
+            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "Some bamboo plants can grow almost a meter in just one day.",
+            "The state of Florida is bigger than England.",
+            "Some penguins can leap 2-3 meters out of the water.",
+            "On average, it takes 66 days to form a new habit.",
+            "Mammoths still walked the earth when the Great Pyramid was being built." };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +36,10 @@ public class FunFactsActivity extends AppCompatActivity implements OnCli{
         mNewFactButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String Fact = "Ostriches can run faster than horses.";
-                mFactTextView.setText(Fact);
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(facts.length);
+                String fact = facts[randomNumber];
+                mFactTextView.setText(fact);
             }
         });
     }
