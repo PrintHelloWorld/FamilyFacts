4c61be27680a5e13b9af4d3063d39c696ad17a69
PrintHelloWorld <blake.mccawe@gmail.com>
2016-07-11 00:00:06 +1200

    Dynamically change the color of the background and button color

--- app/src/main/java/com/bam/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/bam/funfacts/FunFactsActivity.java
@@ -1,9 +1,11 @@
 package com.bam.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -12,6 +14,7 @@ public class FunFactsActivity extends AppCompatActivity {
 
     private TextView mFactTextView;
     private Button mNewFactButton;
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +23,16 @@ public class FunFactsActivity extends AppCompatActivity {
 
         mFactTextView = (TextView) findViewById(R.id.factTextView);
         mNewFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
 
         mNewFactButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mFactTextView.setText(FactBook.getFact());
+
+                int color = ColorWheel.getColor();
+                mRelativeLayout.setBackgroundColor(color);
+                mNewFactButton.setTextColor(color);
             }
         });
     }
