6722f0842050cc2e2616606579bc5c6652989fe5
PrintHelloWorld <blake.mccawe@gmail.com>
2016-07-10 23:45:33 +1200

    Refactor fact generator to its own class.

--- app/src/main/java/com/bam/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/bam/funfacts/FunFactsActivity.java
@@ -13,18 +13,6 @@ public class FunFactsActivity extends AppCompatActivity {
     private TextView mFactTextView;
     private Button mNewFactButton;
 
-    private final String[] facts = {
-            "Ants stretch when they wake up in the morning.",
-            "Ostriches can run faster than horses.",
-            "Olympic gold medals are actually made mostly of silver.",
-            "You are born with 300 bones; by the time you are an adult you will have 206.",
-            "It takes about 8 minutes for light from the Sun to reach Earth.",
-            "Some bamboo plants can grow almost a meter in just one day.",
-            "The state of Florida is bigger than England.",
-            "Some penguins can leap 2-3 meters out of the water.",
-            "On average, it takes 66 days to form a new habit.",
-            "Mammoths still walked the earth when the Great Pyramid was being built." };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,10 +24,7 @@ public class FunFactsActivity extends AppCompatActivity {
         mNewFactButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(facts.length);
-                String fact = facts[randomNumber];
-                mFactTextView.setText(fact);
+                mFactTextView.setText(FactBook.getFact());
             }
         });
     }
